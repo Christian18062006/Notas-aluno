199e4549f4a579da26070d438cca2768e9088abc
Carlos.Christian <carlos.programation@hotmail.comgit config --global user.email carlos.programation@hotmail.com>
2024-03-10 09:52:42 -0300

    variáveis adicionadas e informações solicitadas ao usuário

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace AlunoNotas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string nomeAluno = string.Empty;
+            double nota1, nota2, nota3,notaFinal;
+            Console.WriteLine("Aprovado ou reprovado?");
+            Console.WriteLine("digite o nome do aluno.");
+            nomeAluno = Console.ReadLine();
+            Console.WriteLine("digite a nota do primeiro trimestre.");
+            nota1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("digite a nota do segundo trimestre.");
+            nota2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("digite a nota do terceiro trimestre.");
+            nota3 = double.Parse(Console.ReadLine());
         }
     }
 }
