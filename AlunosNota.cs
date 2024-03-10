a1aae0d92c72d7fcf83909ab3ca012c48fc9ec37
Carlos.Christian <carlos.programation@hotmail.comgit config --global user.email carlos.programation@hotmail.com>
2024-03-10 11:16:28 -0300

    adicionado atributos da classe e método calcula nota.

--- AlunosNota.cs
+++ AlunosNota.cs
@@ -6,5 +6,12 @@ namespace AlunoNotas
 {
     class AlunosNota
     {
+        public string nome = string.Empty;
+        public double nota1, nota2, nota3, notaFinal;
+public double calculaNota()
+        {
+            notaFinal = (nota1 * 3.0 + nota2 * 3.5 + nota3 * 3.5) / (3.0 + 3.5 + 3.5);
+        }
+
     }
 }
