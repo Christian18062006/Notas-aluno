a409996775635a2546a10921709a605c94a495cc
Carlos.Christian <carlos.programation@hotmail.comgit config --global user.email carlos.programation@hotmail.com>
2024-03-10 08:44:25 -0300

    projeto iniciado junto com git ignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlunoNotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
