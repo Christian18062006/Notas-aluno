c6f6f9e86169d1c4462d6afc21a034da954213ff
Carlos.Christian <carlos.programation@hotmail.comgit config --global user.email carlos.programation@hotmail.com>
2024-03-10 11:19:26 -0300

    adicionada a função para verificar aprovação

--- AlunosNota.cs
+++ AlunosNota.cs
@@ -12,6 +12,16 @@ public double calculaNota()
         {
             notaFinal = (nota1 * 3.0 + nota2 * 3.5 + nota3 * 3.5) / (3.0 + 3.5 + 3.5);
         }
-
+        public bool verificaAprovacao()
+        {
+            if (notaFinal>=60)
+            {
+                return (true);
+            }
+            else
+            {
+                return (false);
+            }
+        }
     }
 }
